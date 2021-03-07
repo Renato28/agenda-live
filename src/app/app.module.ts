09c77035e475184fb241cb74d8914b7b08765703
Renato Nobrega <renatonobrega87@gmail.com>
2021-03-07 17:33:00 -0300

    projeto adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,59 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { LiveFormDialogComponent } from './views/home/live-form-dialog/live-form-dialog.component';
+import { LiveListComponent } from './views/home/live-list/live-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LocalDateTimePipe } from './shared/pipe/local-date-time.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LiveFormDialogComponent,
+    LiveListComponent,
+    LocalDateTimePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatProgressBarModule,
+    MatCardModule,
+    HttpClientModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    FormsModule,
+    ReactiveFormsModule
+
+
   ],
-  providers: [],
+  providers: [
+    LocalDateTimePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/views/home/live-list/live-list.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Live } from 'src/app/shared/model/live.model';
+import { LiveService } from 'src/app/shared/service/live.service';
+
+@Component({
+  selector: 'app-live-list',
+  templateUrl: './live-list.component.html',
+  styleUrls: ['./live-list.component.css']
+})
+export class LiveListComponent implements OnInit {
+
+  livesNext: Live[];
+  livesPrevious: Live[];
+  livesNextReady: boolean = false;
+  livesPreviousReady: boolean = false;
+  url: string = '';
+  urlSafe: SafeResourceUrl;
+
+  constructor(
+    private rest: LiveService,
+    public sanitizer: DomSanitizer
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  getLives(){
+    this.rest.getLivesWithFlag('next').subscribe(data => {
+      this.livesNext = data.content;
+      this.livesNext.forEach(live => {
+        live.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(live.liveLink);
+      });
+      this.livesNextReady = true;
+    });
+
+    this.rest.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      this.livesPrevious.forEach(live => {
+        live.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(live.liveLink);
+      });
+      this.livesPreviousReady = true;
+    })
+  }
+
+}
